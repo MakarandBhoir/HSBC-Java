a6cf2dc935feed5e90eba1da85a64bfbeb3dab07
makarandbhoir <makarand.bhoir@live.com>
2023-08-17 14:02:37 +0530

    Initial commit

---
+++ Core Java/Workspace/00-Basic/A.java
@@ -0,0 +1,21 @@
+class FirstProgram
+{
+	public static void main(String args[])
+	{
+		System.out.println("Welcome To HSBC!!!");
+	}
+}
+
+class A{
+	public static void main(String []args)
+	{
+		System.out.println("Welcome To HSBC A!!!");
+	}
+}
+
+class B{
+	public static void main(String ...args)
+	{
+		System.out.println("Welcome To HSBC A!!!");
+	}
+}
